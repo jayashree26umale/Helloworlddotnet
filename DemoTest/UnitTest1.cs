082e2fc46b5074a2addb32ff5e584fad05e5922d
jayashreeumale <jayashreeumale26@gmail.com>
2022-06-01 12:52:33 +0530

    1st commit

---
+++ DemoTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HelloWorld;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Tutorial tp = new Tutorial();
+            Assert.AreEqual(tp.Name, "Jayashre");
+        }
+    }
+}
